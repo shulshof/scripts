9f5ecba160a0b4df3fe5be50b7298528484c8af1
Stephen Hulshof <steve@hulshof.ca>
2017-09-12 22:25:48 -0400

    fixed typo

--- space.py
+++ space.py
@@ -10,7 +10,7 @@ def main():
 
 	# use argparse to acquire host, user and device to look for
 	parser = argparse.ArgumentParser(description='Find ip from space based on hostname')
-	parser.add_argument("-s",'--space',help="IP/Hostname of space")
+	parser.add_argument('-s','--space',help='IP/Hostname of space')
 	parser.add_argument('-u','--user',help='Space username')
 	parser.add_argument('-d','--device',help='Device to acquire IP')
 	args = parser.parse_args()
@@ -47,7 +47,7 @@ def main():
 			print "Host " + DEVICE + " IP is: " + ip
 			exit(0)
 
-			
+
 	# we did not find what we were looking for
 	print "Host: " + DEVICE + " not found!"
 	exit(1)
